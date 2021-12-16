36e2d261bae903fac3e6b9e2d74092552c0cf7c6
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-16 11:49:21 +0100

    aggiunta return al footer

--- src/components/footer-components/FooterBottom.vue
+++ src/components/footer-components/FooterBottom.vue
@@ -1,11 +1,16 @@
 <template>
+
+<footer class="footer_bottom">
+
   <div class="footer_bottom_mdg">
     <div class="container_footer_mdg">
+
       <div class="link_left">
         <p v-for="(infoLink, index) in infoLinks"
         :key="index"
         >{{infoLink.link}}<span>{{infoLink.dash}}</span></p>
       </div>
+
       <div class="link_right">
         <nav>
           <ul>
@@ -15,8 +20,14 @@
           </ul>
         </nav>
       </div>
+        
+    </div>
+
+    <div class="box_return">
+      <a href=""><i class="fas fa-chevron-up"></i></a>
     </div>
   </div>
+</footer>
 </template>
 
 <script>
@@ -33,40 +44,62 @@ export default {
 <style lang="scss">
 @import '../../assets/style/utilities.scss';
 @import '../../assets/style/mixins.scss';
+@import '../../assets/style/vars.scss';
 
-  .footer_bottom_mdg{
-    height: 80px;
-
-    .container_footer_mdg{
-      width: 90%;
-      margin: 0 auto;
-      @include justifyBeetwen();
-
-      .link_left{
-        display: flex;
-
-        p{
-          margin-right: 5px;
-          font-size: 10px;
-          span{
-            margin: 0 10px;
+  .footer_bottom{
+    
+    .footer_bottom_mdg{
+      height: 80px;
+      position: relative;
+  
+      .container_footer_mdg{
+        width: 90%;
+        margin: 0 auto;
+        @include justifyBeetwen();
+  
+        .link_left{
+          display: flex;
+  
+          p{
+            margin-right: 5px;
+            font-size: 10px;
+            span{
+              margin: 0 10px;
+            }
           }
         }
+  
+        .link_right{
+          @include alignCenter();
+  
+          li{
+  
+            i{
+              margin: 0 7px;
+              font-size: 12px;
+              color: #bdbfc4;
+              cursor: pointer;
+            }
+          }
+  
+        }
+  
       }
+    }
+    .box_return{
+      position: absolute;
+      height: 30px;
+      width: 40px;
+      background-color: #474747 ;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      bottom: 0px;
+      right: 55px;
+      @include alignCenter();
+      justify-content: center;
 
-      .link_right{
-        @include alignCenter();
-
-        li{
-
-          i{
-            margin: 0 7px;
-            font-size: 12px;
-            color: #bdbfc4;
-            cursor: pointer;
-          }
-        }
-
+      i{
+        color: $bianco;
       }
     }
   }
